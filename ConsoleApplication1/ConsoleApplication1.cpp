8ee0a7b91b6519b9e152bb36dbb83ea83f2b5045
Dimfster <140328074+Dimfster@users.noreply.github.com>
2023-09-07 08:59:06 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
+    char letter;
+
     std::cout << "Hello World!\n";
+    std::cout << "Input Any letter\n";
+    std::cin >> letter;
+    std::cout << "Input letter:" << letter;
 }
 
